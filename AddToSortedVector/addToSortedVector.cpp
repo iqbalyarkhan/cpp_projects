65018b79da1e1402c4ae867051308073b515c37a
iqbalyarkhan <iqbal.khan@utdallas.edu>
2019-06-16 14:23:06 -0500

    Adds program to insert in a sorted vector: - Makes use of v.insert(v.begin() + n, numToInsert) - Explores idea of incrementing counter as vector size changes

--- AddToSortedVector/addToSortedVector.cpp
+++ AddToSortedVector/addToSortedVector.cpp
@@ -1,30 +1,81 @@
+/*
+
+    This proragm populates a vector by inserting 
+    element in the correct position. Idea is to 
+    learn about: v.insert(v.begin() + n, num);
+
+ */
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-void addToVector(vector<int>&);
+void addToVector(vector<int> &, int);
 
-int main(){
-    while(true){
+int main()
+{
+    vector<int> v;
+    while (true)
+    {
         cout << "Enter a number: " << endl;
         string userInput;
-        getline(cin,userInput);
+        getline(cin, userInput);
         stringstream ss(userInput);
         int num;
-        if (ss >> num){
+        if (ss >> num)
+        {
             string extra;
-            if (ss >> extra){
+            if (ss >> extra)
+            {
                 cout << "enter int only!" << endl;
-            } else {
-                addToVector(v);
+            }
+            else
+            {
+                if (num == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    cout << "Calling add to Vector" << endl;
+                    addToVector(v, num);
+                }
             }
         }
     }
 }
 
-void addToVector(<vector>int v){
-    
+void addToVector(vector<int> &v, int num)
+{
+    if (v.size() == 0)
+    {
+        v.push_back(num);
+    }
+    else
+    {
+        int size = v.size();
+        for (size_t i = 0; i < size; i++)
+        {
+            if (v[i] >= num)
+            {
+                v.insert(v.begin() + i, num);
+                i++;
+            }
+            else if (i == size - 1)
+            {
+                v.push_back(num);
+            }
+        }
+    }
+
+    cout << "So far our vector looks like this: " << endl;
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
 }
